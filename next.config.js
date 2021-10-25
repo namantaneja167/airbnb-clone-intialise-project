809b153f4e004b279341e2166f35d645d32a9953
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2021-10-26 01:31:38 +0530

    next js image error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images:{
-        domains: ["links.papareact.com","upload.wikimedia.org"],
-    },
+        domains: ['upload.wikimedia.org','jsonkeeper.com','links.papareact.com','a0.muscache.com'],
+    }
 };
